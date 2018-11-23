b8e211d99b6fb687cedeb057c32a5d9bb7ede2a3
User <User@DESKTOP-M8FGI40>
2018-11-23 21:53:51 -0200

    arrumei uns bug

--- src/analizadorSintatico/AnalizadorSintatico.java
+++ src/analizadorSintatico/AnalizadorSintatico.java
@@ -12,7 +12,7 @@ import pilha.Token;
 import pilha.Verificador;
 
 import pilha.Inverter;
-import analizadorSintatico.tabelaSintatica;
+
 import javax.swing.JTable;
 
 /**
@@ -21,34 +21,36 @@ import javax.swing.JTable;
  */
 public class AnalizadorSintatico {
 
-    Stack<Token> entrada;
-    Stack<Token> pilhaprod = new Stack();
-    Token producao = new Token();
-    Verificador id = new Verificador();
+    private Stack<Token> entrada;
+    private final Stack<Token> pilhaprod = new Stack();
+    private Token producao = new Token();
+    private final Verificador id = new Verificador();
+    private String erro = "";
+    private int entradacod;
+    private int prodcod;
 
     public AnalizadorSintatico(Stack<Token> entrada) {
         this.entrada = entrada;
 
     }
 
-  public String analisar(JTable tabelaSintatica, JTable tabelaLexica) {
+    public String analisar(JTable tabelaSintatica, JTable tabelaLexica) {
         producao.setNome("programa");
         producao.setCodigo(id.identReservada("programa"));
         pilhaprod.push(producao);
         Inverter inv = new Inverter();
         inv.inverte(entrada);
         entrada = inv.pilha();
-        String erro = "";
-        int entradacod;
-        int prodcod;
+        erro= "sucesso";
 
         while (!pilhaprod.empty()) {
-              entradacod = entrada.peek().getCodigo();
-              prodcod = pilhaprod.peek().getCodigo();
+            entradacod = entrada.peek().getCodigo();
+            prodcod = pilhaprod.peek().getCodigo();
 
             if (entrada.empty()) {
                 erro = "pilha de tokens esta vazia e ainda existe producoes";
-                tabelaSintatica tab = new tabelaSintatica(pilhaprod);
+                inv.inverte(pilhaprod);
+                tabelaSintatica tab = new tabelaSintatica(inv.pilha());
                 tabelaSintatica.setModel(tab);
                 break;
 
@@ -59,17 +61,17 @@ public class AnalizadorSintatico {
                     pilhaprod.pop();
                 } else {
 
-                    erro = ("\n erro na linha: " + entrada.peek().getLinha() + "era esperado um " + pilhaprod.peek().getNome());
-                    tabelaSintatica tabela = new tabelaSintatica(pilhaprod);
+                    erro = ("\n erro na linha: " + entrada.peek().getLinha() + " era esperado um:  " + pilhaprod.peek().getNome());
+                    inv.inverte(pilhaprod);
+                    tabelaSintatica tabela = new tabelaSintatica(inv.pilha());
                     tabelaSintatica.setModel(tabela);
                     tabelaLexica tab = new tabelaLexica(entrada);
-                    tabelaLexica.setModel(tab); 
+                    tabelaLexica.setModel(tab);
                     break;
 
                 }
             } else {
 
-              
                 TabelaProducao tabela = new TabelaProducao();
                 String derivaçao = tabela.getDerivacao(prodcod, entradacod);
                 pilhaprod.pop();
@@ -91,22 +93,7 @@ public class AnalizadorSintatico {
 
             }
         }
-        if(erro!="") {
-           erro +=" ";
-        }
-        else if (!entrada.empty()) {
-            tabelaLexica tab = new tabelaLexica(pilhaprod);
-                tabelaLexica.setModel(tab);
-
-        } else if (pilhaprod.empty() && !entrada.empty()) {
-            erro = "A pilha de producoes esta vazia era esperado um program";
-
-        }
-        else{
-            erro = "executado com sucesso";
-            
-        }
- 
-      return erro;
+         
+        return erro;
     }
 }
